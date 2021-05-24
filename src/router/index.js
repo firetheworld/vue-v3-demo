bd18a437ff9321fc59a70f6d83bbe914d4cb4182
zhengliangkai780 <zhengliangkai780@pingan.com.cn>
2021-05-24 10:07:21 +0800

    feat: add component route

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,11 @@ const routes = [
     component: Home
   },
   {
+    path: '/component',
+    name: 'Component',
+    component: () => import('../views/Component.vue')
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
---
+++ src/views/Component.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="component">
+    <div>Component</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Component',
+}
+</script>
