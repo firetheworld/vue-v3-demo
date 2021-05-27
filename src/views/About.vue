5b5faf147f6ef63ea9495495b13523a74ef87654
zhengliangkai780 <zhengliangkai780@pingan.com.cn>
2021-05-27 15:58:08 +0800

    feat: 异步加载locale资源

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,6 +6,7 @@
     <button @click="changeLang('en')">英文</button>
     <button @click="changeLang('cn')">简体中文</button>
     <button @click="changeLang('zh')">繁體中文</button>
+    <button @click="asynLoadingRes()">异步加载资源</button>
   </div>
 </template>
 
@@ -17,8 +18,11 @@ import { useI18n } from 'vue-i18n';
 export default {
   name: 'Home',
   setup() {
-    const {locale} = useI18n();
-    return {locale};
+    const {locale, setLocaleMessage} = useI18n({
+      inheritLocale: false,
+      useScope: 'global',
+    });
+    return {locale, setLocaleMessage};
   },
   components: {
     HelloI18n
@@ -31,6 +35,12 @@ export default {
   methods: {
     changeLang: function(lang) {
       this.locale = lang;
+    },
+    asynLoadingRes() {
+      const that = this;
+      setTimeout(() => {
+        that.setLocaleMessage('zh', {message: '你好啊，繁體字异步'});
+      }, 2000)
     }
   }
 }
